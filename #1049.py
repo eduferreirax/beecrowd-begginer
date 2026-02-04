3693f544519e12a2e87ecd0835d526c85c3f2738
Eduardo Ferreira <148549393+eduferreirax@users.noreply.github.com>
2026-02-04 01:19:52 -0300

    Add files via upload

---
+++ #1049.py
@@ -0,0 +1,35 @@
+x = str(input())
+y = str(input())
+z = str(input())
+
+#aguia
+if x == 'vertebrado' and y == 'ave' and z == 'carnivoro':
+    print("aguia")
+
+#pomba    
+elif x == 'vertebrado' and y == 'ave' and z == 'onivoro':
+    print("pomba")
+
+#homem
+elif x == 'vertebrado' and y == 'mamifero' and z == 'onivoro':
+    print("homem")
+
+#vaca
+elif x == 'vertebrado' and y == 'mamifero' and z == 'herbivoro':
+    print("vaca")
+
+#pulga
+elif x == 'invertebrado' and y == 'inseto' and z == 'hematofago':
+    print("pulga")
+    
+#lagarta
+elif x == 'invertebrado' and y == 'inseto' and z == 'herbivoro':
+    print("lagarta")
+    
+#sanguessuga
+elif x == 'invertebrado' and y == 'anelideo' and z == 'hematofago':
+    print("sanguessuga")
+
+#minhoca
+elif x == 'invertebrado' and y == 'anelideo' and z == 'onivoro':
+    print("minhoca")
