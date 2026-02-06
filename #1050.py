c1b0b4bfc93c06c2791f245a79a51896e01c2193
Eduardo Ferreira <148549393+eduferreirax@users.noreply.github.com>
2026-02-06 18:21:29 -0300

    Add files via upload

---
+++ #1050.py
@@ -0,0 +1,17 @@
+x = int(input())
+
+ddd = {
+         61: 'Brasilia',
+         71: 'Salvador',
+         11: 'Sao Paulo',
+         21: 'Rio de Janeiro',
+         32: 'Juiz de Fora',
+         19: 'Campinas',
+         27: 'Vitoria',
+         31: 'Belo Horizonte',
+         
+    }
+if x in ddd:    
+    print(ddd[x])
+else:
+    print('DDD nao cadastrado')
