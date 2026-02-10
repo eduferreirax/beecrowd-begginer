c9b1d9ab7905ce4b5a0c70177ddf3997a372905d
Eduardo Ferreira <148549393+eduferreirax@users.noreply.github.com>
2026-02-10 15:39:45 -0300

    Add files via upload

---
+++ #1038.py
@@ -0,0 +1,16 @@
+x, y = map(int, input().split())
+
+if x == 1:
+    price = 4.00
+elif x == 2:
+    price = 4.50
+elif x == 3:
+    price = 5.00
+elif x == 4:
+    price = 2.00
+elif x == 5:
+    price = 1.50
+    
+total = price * y
+
+print(f"Total: R$ {total:.2f}")
