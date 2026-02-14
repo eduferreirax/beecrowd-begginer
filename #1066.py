ef51b1c9b63118f9ab01bdcb0923b64df5d6b8c3
Eduardo Ferreira <148549393+eduferreirax@users.noreply.github.com>
2026-02-14 01:49:14 -0300

    Add files via upload

---
+++ #1066.py
@@ -0,0 +1,21 @@
+even = 0
+odd = 0
+pos = 0
+neg = 0
+
+for i in range(5):
+    n = int(input())
+    
+    if n > 0:
+        pos += 1
+    if n < 0:
+        neg += 1
+    if n % 2 == 0:
+        even += 1
+    if n % 2 != 0:
+        odd += 1
+
+print(f"{even} valor(es) par(es)")
+print(f"{odd} valor(es) impar(es)")
+print(f"{pos} valor(es) positivo(s)")
+print(f"{neg} valor(es) negativo(s)")
